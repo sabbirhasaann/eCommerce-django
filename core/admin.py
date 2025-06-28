2c0ec0d5fa377b08cae522a150a7bfb2c7bf517e
sabbirhasaann <ce2100@mbstu.ac.bd>
2025-06-28 22:45:13 +0600

    create superuser , add user to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
 
 # Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    model = models.User
+
+
+admin.site.register(models.User)
